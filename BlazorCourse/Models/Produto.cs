8ce33d9ec9daa704d589d6aa3e0393af30a1a0a3
Wesley Martins <wesley.mlopes@castgroup.com>
2024-08-12 10:33:12 -0300

    criando uma class e utilizando no front

---
+++ BlazorCourse/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorCourse.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+
+        public decimal ValorEstoqueTotal()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
