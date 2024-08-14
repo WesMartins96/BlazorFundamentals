bb53be9d0e9b79a7fdca7cba9c31544b0caa6f71
Wesley Martins <wesley.mlopes@castgroup.com>
2024-08-14 15:36:49 -0300

    criado API personalizada com uma lista de produtos

---
+++ EcommerceStart/Server/Controllers/ProdutosController.cs
@@ -0,0 +1,90 @@
+﻿using EcommerceStart.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace EcommerceStart.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        [HttpPost("incluir")]
+        public ActionResult Incluir(Produto produto)
+        {
+            if (produto == null)
+                return BadRequest("Produto não foi enviado por parâmetro");
+
+            Produto? produtoAnterior = Banco.Produtos.OrderByDescending(e => e.Id).FirstOrDefault();
+            if (produtoAnterior != null)
+            {
+                produto.Id = produtoAnterior.Id + 1;
+            }
+            else
+            {
+                produto.Id = 1;
+            }
+
+            Banco.Produtos.Add(produto);
+            return Ok();
+        }
+
+        [HttpGet("listar")]
+        public IActionResult Listar(string? nome)
+        {
+            List<Produto> retorno = Banco.Produtos.ToList();
+            if (nome != null)
+            {
+                retorno = Banco.Produtos.Where(e => e.Nome.Contains(nome)).ToList();
+            }
+
+            if (retorno.Count > 0)
+            {
+                return Ok(retorno);
+            }
+            else
+            {
+                return BadRequest("Nenhum produto encontrado!");
+            }
+        }
+
+        [HttpGet("consultar/{id:int}")]
+        public IActionResult Consultar(int id)
+        {
+            Produto? p = Banco.Produtos.Where(e => e.Id == id).FirstOrDefault();
+            if (p == null)
+                return BadRequest("Produto não existe mais na base de dados, deve ter sido removido");
+
+            return Ok(p);
+        }
+
+        [HttpPut("alterar")]
+        public IActionResult Alterar(Produto produto)
+        {
+            if (produto == null)
+                return BadRequest("Produto não foi enviado por parâmetro");
+
+            Produto? p = Banco.Produtos.Where(e => e.Id == produto.Id).FirstOrDefault();
+            if (p == null)
+                return BadRequest("Produto não existe mais na base de dados, deve ter sido removido");
+
+            p.Nome = produto.Nome;
+            p.Preco = produto.Preco;
+            p.Quantidade = produto.Quantidade;
+            p.Imagem = produto.Imagem;
+
+            return Ok();
+        }
+
+        [HttpDelete("excluir/{id:int}")]
+        public IActionResult Excluir(int id)
+        {
+            Produto? p = Banco.Produtos.Where(e => e.Id == id).FirstOrDefault();
+            if (p == null)
+                return BadRequest("Produto não existe mais na base de dados");
+
+            Banco.Produtos.Remove(p);
+            return Ok();
+        }
+    }
+}
